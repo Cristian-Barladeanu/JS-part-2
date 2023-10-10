b79f689db37044ea89cf9d391b94af2c53130e62
Cristian Barladeanu <cbarladeanu@griddynamics.com>
2023-10-10 18:43:44 +0300

    updated with the second task

--- objects/objects.js
+++ objects/objects.js
@@ -4,12 +4,12 @@
 
 let user = {
 
- name: 'John',
+    name: 'John',
 
- age: 30
+    age: 30
 };
 
-const count = (obj) => { 
+const count = (obj) => {
 
     const keys = Object.keys(obj);
 
@@ -17,3 +17,45 @@ const count = (obj) => {
 }
 
 console.log(count(user));
+
+
+// Task 2.
+// There is a salaries object with arbitrary number of salaries.
+
+// Write the function sumSalaries(salaries) that returns the sum of all salaries using Object.values and the for..of loop.
+
+// If salaries is empty, then the result must be 0.
+
+let salaries = {
+
+    "John": 100,
+
+    "Pete": 300,
+
+    "Mary": 250
+
+};
+
+const sumSalaries = (obj) => {
+
+    const salaries = Object.values(obj);
+
+    let temp = 0;
+
+    for (const a of salaries) { 
+        temp += a;
+    }
+
+    return temp;
+
+    // for (const temp of salaries) { 
+
+    //     let ;
+    // }
+
+}
+
+console.log(sumSalaries(salaries)); // 650
+
+
+
